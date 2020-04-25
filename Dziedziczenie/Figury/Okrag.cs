10a1f9f4fbef574d29f657bbcb0ef00b0f889f79
LarryUgwu <64302301+LarryUgwu@users.noreply.github.com>
2020-04-25 14:26:35 +0200

    Update Okrag.cs
    aktualizacja

--- Dziedziczenie/Figury/Okrag.cs
+++ Dziedziczenie/Figury/Okrag.cs
@@ -4,10 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//zmiana z poziomu githuba
 namespace Dziedziczenie.Figury
 {
     class Okrag
     {
         private double srednica;
+        
+        public double ObliczPole()
+        {
+            return 3.14159 * srednica * srednica   
+        }
     }
 }
